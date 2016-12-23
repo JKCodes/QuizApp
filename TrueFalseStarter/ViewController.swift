0f09051710927737d165161952ab46e24b5e663a
Joe Kim <josephkim0224@gmail.com>
2016-12-23 07:59:06 -0500

    fixed erroneous initial commit

---
+++ TrueFalseStarter/ViewController.swift
@@ -0,0 +1,148 @@
+//
+//  ViewController.swift
+//  TrueFalseStarter
+//
+//  Created by Pasan Premaratne on 3/9/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import UIKit
+import GameKit
+import AudioToolbox
+
+class ViewController: UIViewController {
+    
+    let questionsPerRound = 4
+    var questionsAsked = 0
+    var correctQuestions = 0
+    var indexOfSelectedQuestion: Int = 0
+    
+    var gameSound: SystemSoundID = 0
+    
+    struct Trivia {
+        let question: String
+        let firstOption: String
+        let secondOption: String
+        let thirdOption: String
+        let fourthOption: String
+        let correct: Int
+    }
+    
+    let questions = Trivia(question: <#T##String#>, firstOption: <#T##String#>, secondOption: <#T##String#>, thirdOption: <#T##String#>, fourthOption: <#T##String#>, correct: <#T##Int#>)
+    
+    let trivia: [[String : String]] = [
+        ["Question": "This was the only US President to serve more than two consecutive terms.", "Choice 1": "George Washington", "Choice 2": "Franklin D. Roosevelt", "Choice 3": "Woodrow Wilson", "Choice 4": "Andrew Jackson", "Correct": "2"],
+        ["Question": "Which of the following countries has the most residents", "Choice 1", "Nigeria", "Choice 2": "Russia", "Choice 3": "Iran", "Choice 4": "Vietnam"]
+        ["Question": "Only female koalas can whistle", "Answer": "False"],
+        ["Question": "Blue whales are technically whales", "Answer": "True"],
+        ["Question": "Camels are cannibalistic", "Answer": "False"],
+        ["Question": "All ducks are birds", "Answer": "True"]
+    ]
+    
+    @IBOutlet weak var questionField: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var firstChoiceButton: UIButton!
+    @IBOutlet weak var secondChoiceButton: UIButton!
+    @IBOutlet weak var thirdChoiceButton: UIButton!
+    @IBOutlet weak var fourthChoiceButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadGameStartSound()
+        // Start game
+        playGameStartSound()
+        displayQuestion()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func displayQuestion() {
+        indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextInt(upperBound: trivia.count)
+        let questionDictionary = trivia[indexOfSelectedQuestion]
+        questionField.text = questionDictionary["Question"]
+        playAgainButton.isHidden = true
+    }
+    
+    func displayScore() {
+        // Hide the answer buttons
+        firstChoiceButton.isHidden = true
+        secondChoiceButton.isHidden = true
+        thirdChoiceButton.isHidden = true
+        fourthChoiceButton.isHidden = true
+        // Display play again button
+        playAgainButton.isHidden = false
+        
+        questionField.text = "Way to go!\nYou got \(correctQuestions) out of \(questionsPerRound) correct!"
+        
+    }
+    
+    @IBAction func checkAnswer(_ sender: UIButton) {
+        // Increment the questions asked counter
+        questionsAsked += 1
+        
+        let selectedQuestionDict = trivia[indexOfSelectedQuestion]
+        let correctAnswer = selectedQuestionDict["Answer"]
+        
+        if (sender === trueButton &&  correctAnswer == "True") || (sender === falseButton && correctAnswer == "False") {
+            correctQuestions += 1
+            questionField.text = "Correct!"
+        } else {
+            questionField.text = "Sorry, wrong answer!"
+        }
+        
+        loadNextRoundWithDelay(seconds: 2)
+    }
+    
+    func nextRound() {
+        if questionsAsked == questionsPerRound {
+            // Game is over
+            displayScore()
+        } else {
+            // Continue game
+            displayQuestion()
+        }
+    }
+    
+    @IBAction func playAgain() {
+        // Show the answer buttons
+        firstChoiceButton.isHidden = false
+        secondChoiceButton.isHidden = false
+        thirdChoiceButton.isHidden = false
+        fourthChoiceButton.isHidden = false
+        
+        questionsAsked = 0
+        correctQuestions = 0
+        nextRound()
+    }
+    
+
+    
+    // MARK: Helper Methods
+    
+    func loadNextRoundWithDelay(seconds: Int) {
+        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
+        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
+        // Calculates a time value to execute the method given current time and delay
+        let dispatchTime = DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)
+        
+        // Executes the nextRound method at the dispatch time on the main queue
+        DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
+            self.nextRound()
+        }
+    }
+    
+    func loadGameStartSound() {
+        let pathToSoundFile = Bundle.main.path(forResource: "GameSound", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: pathToSoundFile!)
+        AudioServicesCreateSystemSoundID(soundURL as CFURL, &gameSound)
+    }
+    
+    func playGameStartSound() {
+        AudioServicesPlaySystemSound(gameSound)
+    }
+}
+
